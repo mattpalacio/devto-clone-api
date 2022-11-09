a6735718ed0707548369c3be96d23c329f5e4526
Matthew Palacio <mpalacio@catalyte.io>
2022-11-09 13:13:31 -0800

    adds CORS config

---
+++ DevtoClone.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace DevtoClone.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+        }
+    }
+}
