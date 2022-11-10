10f5b4ed79bd570d1c26bb1fa155788d2c26569e
Matthew Palacio <mpalacio@catalyte.io>
2022-11-10 09:47:48 -0800

    sets up repository context

--- DevtoClone.Entities/RepositoryContext.cs
+++ DevtoClone.Entities/RepositoryContext.cs
@@ -1,7 +1,17 @@
-﻿namespace DevtoClone.Entities
-{
-    public class RepositoryContext
-    {
+﻿using DevtoClone.Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
+namespace DevtoClone.Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Post>? Posts { get; set; }
+        public DbSet<Tag>? Tags { get; set; }
     }
 }
