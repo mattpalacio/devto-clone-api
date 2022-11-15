b28b2f098f08de3f69fb3af564a254712ab7d2e7
Matthew Palacio <mpalacio@catalyte.io>
2022-11-15 15:33:21 -0800

    refactors helper method in post service

--- DevtoClone.Core/Services/PostService.cs
+++ DevtoClone.Core/Services/PostService.cs
@@ -25,17 +25,8 @@ namespace DevtoClone.Core.Services
 
         private async Task<IEnumerable<Tag>> GetTagsAsync(string[] postTags)
         {
-            IEnumerable<Tag> newTags;
             var existingTags = await _unitOfWork.Tags.GetAsync(filter: x => postTags.Any(t => t == x.Name));
-
-            if(existingTags.Any())
-            {
-                newTags = postTags.Where(t => existingTags.Any(et => et.Name != t)).Select(tag => new Tag { Name = tag });
-            }
-            else
-            {
-                newTags = postTags.Select(tag => new Tag { Name = tag });
-            }
+            var newTags = postTags.Where(t => existingTags.Any(et => et.Name != t)).Select(tag => new Tag { Name = tag });
 
             if(newTags.Any())
             {
@@ -85,6 +76,8 @@ namespace DevtoClone.Core.Services
                 if(postTags.Any())
                 {
                     var tags = await GetTagsAsync(postTags);
+
+                    post.Tags = new Collection<Tag>();
                     foreach (var tag in tags)
                     {
                         post.Tags.Add(tag);
@@ -136,8 +129,14 @@ namespace DevtoClone.Core.Services
         {
             try
             {
-                _unitOfWork.Posts.Delete(id);
+                var existingPost = await _unitOfWork.Posts.GetByIdAsync(id);
 
+                if (existingPost is null)
+                {
+                    throw new ArgumentNullException(nameof(existingPost));
+                }
+
+                _unitOfWork.Posts.Delete(existingPost);
                 await _unitOfWork.SaveAsync();
             }
             catch (Exception ex)
